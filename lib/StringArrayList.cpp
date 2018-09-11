82ec4e1f01da8bf2b163b3c8133d33a0aa39f053
Harivansh Mareddy <harivanshmareddy@Harivanshs-MacBook-Pro.local>
2018-09-11 14:25:29 -0400

    changed the file structure

---
+++ lib/StringArrayList.cpp
@@ -0,0 +1,27 @@
+#include <stdio.h>s
+#include<string>
+#include <iostream>
+
+using namespace std;
+
+push_back(string) {
+
+}
+
+
+get(n) {
+
+}
+
+length() {
+
+}
+
+remove_front() {
+
+}
+
+int main {
+
+  return 0;
+}
