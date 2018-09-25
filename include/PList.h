2b0f8f6718d9211c44146f653aa7bde39d37b0a4
Harivansh Mareddy <harivanshmareddy@Harivanshs-MacBook-Pro.local>
2018-09-25 13:39:43 -0400

    Updated Makefiles, Plist_test and Plist.h to private for node *data

--- include/PList.h
+++ include/PList.h
@@ -7,6 +7,7 @@ public:
   virtual void print(){ printf("object!\n");  };
 };
 
+//Integer Object
 class Integer: public Object {
 public:
   int data;
@@ -16,7 +17,7 @@ public:
   }
 
 };
-
+//Doubles Object
 class Doubles: public Object {
 public:
   double data;
@@ -25,7 +26,7 @@ public:
      printf("%f\n", data);
   }
 };
-
+//Double Strings
 class Strings: public Object {
 public:
   char* data;
@@ -35,13 +36,14 @@ public:
   }
 };
 
+//List created here
 class PList {
 private:
-  class ListNode {
+  class ListNode { //Node Created
   private:
     ListNode *next;
+    Object *data; //Moved to Public bc of access errors
   public:
-    Object *data;
     ListNode (Object *input, ListNode *n_value = NULL){
     data = input;
     next = n_value;
--- tests/PList_Test.cpp
+++ tests/PList_Test.cpp
@@ -5,38 +5,57 @@
 
 int main() {
 
-  PList new_list;
+  PList new_list; //list Object created
+
+  //------------------------ assigned variables names
   Object* num = new Integer(4);
   Object* dollars = new Doubles(10.2);
   Object* Letters = new Strings((char *)"BOBA");
   Object* dollars_2 = new Doubles(1.49);
   Object* num_2 = new Integer(12);
-  Object* Letters_2 = new Strings((char *)"Bubble_Tea");
+  Object* Letters_3 = new Strings((char *)"Jasmine");
+  Object* Letters_4 = new Strings((char *)"Green");
+  Object* Letters_5 = new Strings((char *)"Pink");
+
+  //------------------------ Appended a list to inster-----
+  new_list.append(num);
+  new_list.append(dollars);
+  new_list.append(Letters);
+  new_list.append(dollars_2);
+  new_list.append(num_2);
+  new_list.append(Letters_3);
+  new_list.append(Letters_4);
+  new_list.append(Letters_5);
+// inistilaized iterator to being
+  PList::iterator it = new_list.begin();
+
+//Print all in the list
+  while (!it.end()) {
+    it.node->getItem()->print();
+    it.increment();
+   }
+
+   printf("---------------\n");
+   it = new_list.begin();
+   it.increment();
+   new_list.removeAfter(it);
+   it = new_list.begin();
 
 
-    new_list.append(num);
-    new_list.append(dollars);
-    new_list.append(Letters);
-    new_list.append(dollars_2);
-    new_list.append(num_2);
-    new_list.append(Letters_2);
-
-    PList::iterator it = new_list.begin();
-    while (!it.end()) {
-      it.node->getItem()->print();
-      it.increment();
-     }
-
-     it = new_list.begin();
-
+   while(!it.end()) {
+     it.node->getItem()->print();
      it.increment();
-     new_list.removeAfter(it);
-     it = new_list.begin(); 
-     //printf("Print List Again\n");
-     while(!it.end()) {
-       it.node->getItem()->print();
-       it.increment();
-    }
+  }
+  printf("---------------\n");
+  it = new_list.begin();
+  it.increment();
+  new_list.removeAfter(it);
+  it = new_list.begin();
+
+  while(!it.end()) {
+    it.node->getItem()->print();
+    it.increment();
+ }
 
   return 0;
 }
