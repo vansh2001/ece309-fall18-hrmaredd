a3496eeb0deb7f2335ab51fe3aa9660aa9c489ce
Harivansh Mareddy <harivanshmareddy@Harivanshs-MacBook-Pro.local>
2018-10-10 15:22:47 -0400

    Hw5 question 1

---
+++ include/IntegerSetArray.h
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include<string>
+#include <iostream>
+
+using namespace std;
+
+class IntegerSet {
+protected:
+   int size;
+   int hash(int key) const { return (key * 997) % size; }
+public:
+   IntegerSet(int htsize):size(htsize) {}
+   virtual bool insert(int) = 0;
+   virtual bool search(int) const = 0;
+   virtual void remove(int) = 0;
+};
+
+class IntegerSetArray: public IntegerSet {
+protected:
+  int *table;
+public:
+  int filled;
+  IntegerSetArray(int x);
+  virtual bool insert(int) override;
+  virtual bool search(int) const override;
+  virtual void remove(int) override;
+};
---
+++ lib/IntegerSetArray.cpp
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include<string>
+#include <iostream>
+#include "IntegerSetArray.h"
+
+
+using namespace std;
+
+
+IntegerSetArray::IntegerSetArray(int x):IntegerSet(x) {
+  filled = 0;
+  table = new int[x];
+  for(int i = 0; i < x; i++){
+    table[i] = 0;
+  }
+}
+
+bool IntegerSetArray::insert(int data){
+  // insert into table if there’s no collision
+   if (filled == size) {
+      return false;
+   }
+   table[filled++] = data;
+   return true;
+}
+
+bool IntegerSetArray::search(int data) const {
+  for(int i = 0; i < filled; i++){
+    if(table[i] == data) {
+      return true;
+    }
+  }
+  return false;
+}
+
+void  IntegerSetArray::remove(int data) {
+  for(int i = 0; i < filled; i++){
+    if(table[i] == data) {
+      for(int j = i; j < filled; j++ ) {
+        table[i] = table[j+1];
+      }
+      filled--;
+      i--;
+    }
+  }
+  for(int k = filled; k < size; k++) {
+    table[k] = 0;
+  }
+}
