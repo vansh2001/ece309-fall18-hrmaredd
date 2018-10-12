91f0321c2bc7fca8d17d034ba24b3ab6cd907d93
Harivansh Mareddy <harivanshmareddy@Harivanshs-MacBook-Pro.local>
2018-10-11 21:43:58 -0400

    updated for travis

--- lib/IntArray.cpp
+++ lib/IntArray.cpp
@@ -39,16 +39,16 @@ IntArray& IntArray::operator+=(const IntArray &rhs){
 }
 
 IntArray IntArray::operator + (const IntArray &rhs) const {
-    IntArray result(length); // make object to hold return value,
+    IntArray ans(length); // make object to hold return value,
     // make long enough to hold this object’s data
     int i=0;
     for(i=0; i<length && i<rhs.length; i++) {
-        result.array[i] = array[i] + rhs.array[i];
+        ans.array[i] = array[i] + rhs.array[i];
     }
     for(;i<length;i++) {// if rhs.length is shorter, copy rest of array
-        result.array[i] = array[i];
+        ans.array[i] = array[i];
     }
-    return result;
+    return ans;
 }
 
 IntArray IntArray::operator-(const IntArray &rhs) const {
