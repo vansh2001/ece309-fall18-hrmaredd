255e33e8897373356f4d8af17cb036ff8781f626
Harivansh Mareddy <harivanshmareddy@Harivanshs-MBP.attlocal.net>
2018-12-02 12:44:27 -0500

    update randomHash

--- lib/randomHash_Iterator.cpp
+++ lib/randomHash_Iterator.cpp
@@ -5,36 +5,6 @@
 
 using namespace std;
 
-
-// Hash Table with Linear Probing
-class IntegerSetHT : public IntegerSet {
-protected:
-  int *table;
-  int probeDistance;
-  const int empty_since_start = -2;
-  const int empty_after_removal = -1;
-public:
-  int c = 0; //Collisions
-  int f = 0; //Fails
-   IntegerSetHT(int htsize);
-   virtual bool insert(int) override;
-   virtual bool search(int) const override;
-   virtual void remove(int) override;
-
-class iterator {
-  int passed_size;
-  int *passed_table;
-  int index;
-public:
-  iterator (int s, int *l);
-  int getInt();
-  void increment();
-  bool end();
-
-  };
-  iterator begin() { return iterator(size, table); }
-};
-
  IntegerSetHT::iterator::iterator(int s, int *l) {
   passed_size = s;
   passed_table= l;
