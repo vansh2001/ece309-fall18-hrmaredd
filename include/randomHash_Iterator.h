91894bde1d3b5be16e4f5e180a8a14cc62278929
Harivansh Mareddy <harivanshmareddy@Harivanshs-MBP.attlocal.net>
2018-12-02 12:42:17 -0500

    upodated header with class setHT

--- include/randomHash_Iterator.h
+++ include/randomHash_Iterator.h
@@ -14,3 +14,32 @@ public:
    virtual bool search(int) const = 0;
    virtual void remove(int) = 0;
 };
+
+// Hash Table with Linear Probing
+class IntegerSetHT : public IntegerSet {
+protected:
+  int *table;
+  int probeDistance;
+  const int empty_since_start = -2;
+  const int empty_after_removal = -1;
+public:
+  int c = 0; //Collisions
+  int f = 0; //Fails
+   IntegerSetHT(int htsize);
+   virtual bool insert(int) override;
+   virtual bool search(int) const override;
+   virtual void remove(int) override;
+
+class iterator {
+  int passed_size;
+  int *passed_table;
+  int index;
+public:
+  iterator (int s, int *l);
+  int getInt();
+  void increment();
+  bool end();
+
+  };
+  iterator begin() { return iterator(size, table); }
+};
