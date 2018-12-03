6ded887b0fe4f8738c2a8af03cd5ab0769dd8bff
Harivansh Mareddy <harivanshmareddy@Harivanshs-MacBook-Pro.local>
2018-12-03 13:49:57 -0500

    namespace test 2

--- lib/randomHash.cpp
+++ lib/randomHash.cpp
@@ -5,111 +5,111 @@
 using namespace std;
 namespace ece309 {
 
-// IntegerSet abstract base class to demonstrate hash table concepts
-class IntegerSet {
-protected:
-   int size;
-   int hash(int key) const {
-     if(((key * 997) % size) < 0) {
+  // IntegerSet abstract base class to demonstrate hash table concepts
+  class IntegerSet {
+  protected:
+     int size;
+     int hash(int key) const {
+       if(((key * 997) % size) < 0) {
 
-       return -((key * 997) % size);
+         return -((key * 997) % size);
+       }
+       return (key * 997) % size;
      }
-     return (key * 997) % size;
-   }
-public:
-   IntegerSet(int htsize):size(htsize) {}
-   virtual bool insert(int) = 0;
-   virtual bool search(int) const = 0;
-   virtual void remove(int) = 0;
-};
+  public:
+     IntegerSet(int htsize):size(htsize) {}
+     virtual bool insert(int) = 0;
+     virtual bool search(int) const = 0;
+     virtual void remove(int) = 0;
+  };
 
-// Hash Table with Linear Probing
-class IntegerSetHT : public IntegerSet {
-protected:
-  int *table;
-  int probeDistance;
-  const int empty_since_start = -2;
-  const int empty_after_removal = -1;
-public:
-  int c = 0; //Collisions
-  int f = 0; //Fails
-   IntegerSetHT(int htsize);
-   virtual bool insert(int) override;
-   virtual bool search(int) const override;
-   virtual void remove(int) override;
-};
+  // Hash Table with Linear Probing
+  class IntegerSetHT : public IntegerSet {
+  protected:
+    int *table;
+    int probeDistance;
+    const int empty_since_start = -2;
+    const int empty_after_removal = -1;
+  public:
+    int c = 0; //Collisions
+    int f = 0; //Fails
+     IntegerSetHT(int htsize);
+     virtual bool insert(int) override;
+     virtual bool search(int) const override;
+     virtual void remove(int) override;
+  };
 
-// can only hold positive integers
-IntegerSetHT::IntegerSetHT(int htsize)
-:IntegerSet(htsize)
-{
-  probeDistance = 20;
-   table = new int[size];
-   for(int i=0; i<size; i++)
-      table[i] = empty_since_start;  // -1 means empty
-}
-
-bool IntegerSetHT::insert(int data)
-{
-  int index = hash(data);
-  int bucketsProbed = 0;
-  while( bucketsProbed++ < probeDistance )  // bucketsProbed less than probeDistance
+  // can only hold positive integers
+  IntegerSetHT::IntegerSetHT(int htsize)
+  :IntegerSet(htsize)
   {
-   if ( table[index] < 0 )
-   {
-     // if the entry is not being used, put the
-    // data there
-     table[ index ] = data;
-     // cout << "# of c = " <<  c << "\n";
-     // cout << "# of f = " <<  f << "\n";
-
-     return true;
-   }
-   if(bucketsProbed == 2) {
-     c++;
-   }
-   index = (index+1) % size;
+    probeDistance = 20;
+     table = new int[size];
+     for(int i=0; i<size; i++)
+        table[i] = empty_since_start;  // -1 means empty
   }
-  // otherwise give up
-  f++;
-  // cout << "Number of c = " <<  c << "\n";
-  // cout << "Number of f = " <<  f << "\n";
 
-   return false;
-}
-
-bool IntegerSetHT::search(int data) const
-{
-  int index = hash(data);
-  int bucketsProbed = 0;
-  while( table[index] != empty_since_start &&
-         bucketsProbed++ < probeDistance )
+  bool IntegerSetHT::insert(int data)
   {
-   if ( table[index] == data )
-   {
-     return true;
-   }
-   index = (index+1) % size;
-  }
-  // data can only be in one location, check it
-  return false;
-}
+    int index = hash(data);
+    int bucketsProbed = 0;
+    while( bucketsProbed++ < probeDistance )  // bucketsProbed less than probeDistance
+    {
+     if ( table[index] < 0 )
+     {
+       // if the entry is not being used, put the
+      // data there
+       table[ index ] = data;
+       // cout << "# of c = " <<  c << "\n";
+       // cout << "# of f = " <<  f << "\n";
 
-void IntegerSetHT::remove(int data)
-{
-  int index = hash(data);
-  int bucketsProbed = 0;
-  while( table[index] != empty_since_start &&
-         bucketsProbed++ < probeDistance )
-  {
-   if ( table[index] == data )
-   {
-     table[index] = empty_after_removal;
-     //return;
-   }
-   index = (index+1) % size;
+       return true;
+     }
+     if(bucketsProbed == 2) {
+       c++;
+     }
+     index = (index+1) % size;
+    }
+    // otherwise give up
+    f++;
+    // cout << "Number of c = " <<  c << "\n";
+    // cout << "Number of f = " <<  f << "\n";
+
+     return false;
   }
-}
+
+  bool IntegerSetHT::search(int data) const
+  {
+    int index = hash(data);
+    int bucketsProbed = 0;
+    while( table[index] != empty_since_start &&
+           bucketsProbed++ < probeDistance )
+    {
+     if ( table[index] == data )
+     {
+       return true;
+     }
+     index = (index+1) % size;
+    }
+    // data can only be in one location, check it
+    return false;
+  }
+
+  void IntegerSetHT::remove(int data)
+  {
+    int index = hash(data);
+    int bucketsProbed = 0;
+    while( table[index] != empty_since_start &&
+           bucketsProbed++ < probeDistance )
+    {
+     if ( table[index] == data )
+     {
+       table[index] = empty_after_removal;
+       //return;
+     }
+     index = (index+1) % size;
+    }
+  }
 }
 
 int main() {
